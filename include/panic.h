ac4cfb2b48bf16950762a29a6d9520d8c757bf08
Nishat Munshi <nishatmunshi9@gmail.com>
2026-01-24 16:48:04 +0000

    implemented variadic panic()

--- include/panic.h
+++ include/panic.h
@@ -1,6 +1,15 @@
 #ifndef PANIC_H
 #define PANIC_H
 
-[[noreturn]] void panic(char* message);
+#include "io.h"
+#include "linux.h"
+
+#define panic(format, ...) \
+    do { \
+        eprintf("mcc: error: "); \
+        eprintf((format)__VA_OPT__(, )__VA_ARGS__); \
+        eprintf("\n"); \
+        linux_exit(LINUX_EXIT_FAILURE); \
+    } while(0)
 
 #endif  // PANIC_H
--- src/panic.c
+++ src/panic.c
@@ -1,8 +1,2 @@
 #include <io.h>
 #include <linux.h>
-#include <panic.h>
-
-[[noreturn]] void panic(char* message) {
-    printf("mcc: error: %s\n", message);
-    linux_exit(LINUX_EXIT_FAILURE);
-}
